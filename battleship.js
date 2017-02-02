f00a3c7bb4e59e40e2999ce7795fd9d93fa6dbbe
rrutherfordd <rrutherfordd@hotmail.com>
2017-02-02 05:20:13 -0800

    homework03

--- battleship.js
+++ battleship.js
@@ -16,22 +16,22 @@ alert("location3: " + location3);
 */
 
 while (iSunk == false) {
-  guess = prompt("Ready, aim, fire! (enter a number from 0 - 6): ");
+  guess = window.prompt("Ready, aim, fire! (enter a number from 0 - 6): ");
   if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
+    window.alert("Please enter a valid cell number!");
   } else {
     guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
-      alert("HIT!");
+      window.alert("HIT!");
       hits = hits + 1;
       if (hits == 3) {
         iSunk = true;
-        alert("You sank my battleship!");
+        window.alert("You sank my battleship!");
       }
     } else {
-        alert("MISS");
+      window.alert("MISS");
     }
   }
 }
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
-alert(stats);
+window.alert(stats);
